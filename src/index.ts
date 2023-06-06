98a237c78d076eb6b6b3694f6fb85b716d56fd10
Rebekah Higgins <rebekahapelt@gmail.com>
2023-06-06 16:06:02 +1000

    Refactor index

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,34 @@
 
 import { Timer, TIMER_EVENT_NAME } from './timer';
 import { Printer } from './print';
-import { Fibonacci } from './fibonacci';
 import { Storage } from './storage';
 import { Question } from './question';
 
+const SECONDS_QUESTION_TEXT = "Please input the amount of time in seconds between emitting numbers and their frequency";
+const FIRST_FIB_QUESTION_TEXT = "Please enter the first number";
+
 const storage = Storage.getInstance();
-const fibonacci = Fibonacci.getInstance();
 const timer = Timer.getInstance();
 
 const question = new Question();
 
-question.askNumberOfSecondsQuestion("Please input the amount of time in seconds between emitting numbers and their frequency").then((answer: number) => {
-    if (answer) {
-        timer.startTimer(Number(answer));
-        timer.timerEvent.on(TIMER_EVENT_NAME, () => {
-            Printer.print(storage.toPrint());
-        });
+function startTimer(numberOfTimerSeconds: number): void {
+    timer.startTimer(Number(numberOfTimerSeconds));
+    timer.timerEvent.on(TIMER_EVENT_NAME, () => {
+        Printer.print(storage.toPrint());
+    });
+}
+
+async function startProgram(){
+    let numberOfTimerSeconds: number = await question.askNumberOfSecondsQuestion(SECONDS_QUESTION_TEXT);
+
+    if (numberOfTimerSeconds) {
+        startTimer(numberOfTimerSeconds);
     }
 
-    question.askFibNumberQuestion('Please enter the first number').then(() => {
-        timer.pauseTimer();
-    });
-})
+    await question.askFibNumberQuestion(FIRST_FIB_QUESTION_TEXT);
+
+    timer.pauseTimer();
+}
+
+startProgram();
